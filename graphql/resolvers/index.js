6b687069a6c36124393d71757fc746da253dc54e
Cillian <hynesy23@hotmail.com>
2019-12-18 12:24:11 +0000

    add transformBooking func to correctly confire returned bookings

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,7 +2,19 @@ const Event = require("../../schema-models/event-model");
 const User = require("../../schema-models/user-model");
 const Booking = require("../../schema-models/booking-model");
 const bcrypt = require("bcrypt");
+const { events } = require("../resolvers/event-resolvers");
+const { dateToString } = require("../../utils/dates");
 
+const transformBooking = booking => {
+  return {
+    ...booking,
+    _id: booking.id,
+    user: formattedUser(booking.user),
+    event: formatSingleEvent(booking.event),
+    createdAt: dateToString(booking.createdAt),
+    updatedAt: dateToString(booking.updatedAt)
+  };
+};
 // Below, this function will take an array of event IDs that comes from a user objects 'createdEvents' key. It will return a list of events by that user. Then, it will format the events so that the event.createdBy field is populated with a full user object. We get this object from the formattedUser function.
 
 const formattedEvents = eventIds => {
@@ -11,7 +23,7 @@ const formattedEvents = eventIds => {
       return events.map(event => {
         return {
           ...event._doc,
-          date: new Date(event._doc.date).toISOString(), //Need to do this as date is saved to Mongo as a date object. When parsed back to string it is unreadable.
+          date: dateToString(event._doc.date), //Need to do this as date is saved to Mongo as a date object. When parsed back to string it is unreadable.
           createdBy: formattedUser(event.createdBy)
         };
       });
@@ -52,29 +64,7 @@ const formattedUser = userId => {
 
 module.exports = {
   // Points to where all resolver functions live. rootValue also known as RESOLVER
-  events: () => {
-    return (
-      Event.find()
-        //.populate("createdBy")  --> Orginally .populate() so createdBy field would be populated with user data. Use 'formattedUser' func to populate this data instead. Means now I've got a full user object for createdBy, rather than just user ID.
-        .then(results => {
-          return results.map(result => {
-            console.log(result, "RESULT");
-            console.log(result._doc, "RESULT._DOC");
-
-            const fullEvent = {
-              ...result._doc,
-              date: new Date(result._doc.date).toISOString(),
-              createdBy: formattedUser(result.createdBy)
-            };
-            return fullEvent;
-          });
-        })
-        .catch(err => {
-          console.log(err);
-          throw err;
-        })
-    );
-  },
+  events: events,
   users: () => {
     return User.find()
       .populate("createdEvents")
@@ -87,18 +77,16 @@ module.exports = {
       });
   },
   bookings: () => {
-    return Booking.find().then(bookings => {
-      return bookings.map(booking => {
-        const fullBooking = {
-          ...booking._doc,
-          user: formattedUser(booking.user),
-          event: formatSingleEvent(booking.event),
-          createdAt: new Date(booking._doc.createdAt.toISOString()),
-          updatedAt: new Date(booking._doc.updatedAt.toISOString())
-        };
-        return fullBooking;
+    return Booking.find()
+      .then(bookings => {
+        return bookings.map(booking => {
+          return transformBooking(booking);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        throw err;
       });
-    });
   },
   createEvent: ({ eventInput }) => {
     const { title, description, price, date } = eventInput;
@@ -106,7 +94,7 @@ module.exports = {
       title: title,
       description: description,
       price: +price,
-      date: new Date(date),
+      date: dateToString(date),
       createdBy: "5df653999d294a2e06cfde03"
     });
     return event
@@ -126,7 +114,10 @@ module.exports = {
         };
         return fullEvent;
       })
-      .catch(console.log);
+      .catch(err => {
+        console.log(err);
+        throw err;
+      });
   },
   createUser: ({ userInput }) => {
     const { email, password } = userInput;
@@ -164,14 +155,7 @@ module.exports = {
         return booking.save();
       })
       .then(booking => {
-        return {
-          ...booking,
-          _id: booking.id,
-          user: formattedUser(booking.user),
-          event: formatSingleEvent(foundEvent),
-          createdAt: new Date(booking.createdAt.toISOString()),
-          updatedAt: new Date(booking.updatedAt.toISOString())
-        };
+        return transformBooking(booking);
       })
       .catch(err => {
         console.log(err, "error");
