96798e6001d6ef3eb947ae24449bcb57444187b3
Cillian <hynesy23@hotmail.com>
2019-12-20 12:40:20 +0000

    add pages and components folders, and initialise Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Router } from "@reach/router";
+import AuthPage from "./pages/AuthPage";
+import BookingsPage from "./pages/BookingsPage";
+import EventsPage from "./pages/EventsPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <AuthPage path="/auth" />
+        <BookingsPage path="/bookings" />
+        <EventsPage path="/events" />
+      </Router>
     </div>
   );
 }
