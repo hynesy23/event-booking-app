4d9cee530c4842467b9de06e1396f490e3f6f2c6
Cillian <hynesy23@hotmail.com>
2020-01-05 13:18:16 +0000

    add inline validation for valid user email and password

--- frontend/src/pages/auth/AuthPage.jsx
+++ frontend/src/pages/auth/AuthPage.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 
 import * as styles from "./AuthPage.module.css";
+import * as api from "../../api";
+import Forms from "./Forms";
 
 export default class AuthPage extends Component {
   state = {
     email: "",
-    password: ""
+    password: "",
+    signup_email: "",
+    siginup_password: "",
+    signin: "Please SIGNIN to continue:",
+    signup: "Or, if you have not already done so, please SIGNUP to continue:",
+    invalidPassword: false,
+    invalidEmail: false
   };
 
   handleChange = event => {
@@ -16,40 +24,107 @@ export default class AuthPage extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    if (!email.includes("@")) {
+      this.setState({ invalidEmail: true });
+    } else if (password.length < 8) {
+      this.setState({ invalidPassword: true, invalidEmail: false });
+    } else {
+      // api.logIn(signin_email, signin_password).then(result => {
+      //   console.log(result, "result log");
+      // });
+      this.setState({
+        email: "",
+        password: "",
+        invalidPassword: false,
+        invalidEmail: false
+      });
+    }
   };
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, invalidEmail, invalidPassword } = this.state;
     return (
-      <form onSubmit={this.handleSubmit} className={styles.auth_form}>
-        <div className={styles.form_control}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            value={email}
-            required
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className={styles.form_control}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={password}
-            required
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className={styles.form_actions}>
-          <button type="submit">Submit</button>
-          <button type="button">Signup instead</button>
-        </div>
-      </form>
+      <>
+        <form onSubmit={this.handleSubmit} className={styles.auth_form}>
+          <p>Please SIGNIN to continue:</p>
+          <div className={styles.form_control}>
+            <label>
+              Email
+              <input
+                type="text"
+                name="email"
+                value={email}
+                required
+                onChange={this.handleChange}
+              />
+            </label>
+          </div>
+          {invalidEmail && (
+            <p className={styles.invalid}>This is not a valid email address</p>
+          )}
+          <div className={styles.form_control}>
+            <label>
+              Password
+              <input
+                type="password"
+                name="password"
+                value={password}
+                required
+                onChange={this.handleChange}
+              />
+            </label>
+          </div>
+          {invalidPassword && (
+            <p className={styles.invalid}>Your password is not long enough</p>
+          )}
+          <div className={styles.form_actions}>
+            <button type="submit">Signin</button>
+          </div>
+        </form>
+        {/* <form onSubmit={this.handleSubmit} className={styles.auth_form}>
+          <p>Or, if you have not already done so, please SIGNUP to continue:</p>
+          <div className={styles.form_control}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              required
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className={styles.form_control}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              required
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className={styles.form_actions}>
+            <button type="submit">Signup</button>
+          </div>
+        </form> */}
+        {/* <Forms
+          action={signin}
+          email={email}
+          password={password}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
+        <Forms
+          action={signup}
+          email={email}
+          password={password}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        /> */}
+      </>
     );
   }
 }
