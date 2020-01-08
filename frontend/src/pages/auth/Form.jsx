897426c75d9bdca280a874d16ada2f15bced898c
Cillian <hynesy23@hotmail.com>
2020-01-08 17:17:22 +0000

    add reusable Form component

---
+++ frontend/src/pages/auth/Form.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import * as styles from "./AuthPage.module.css";
+
+export default function Form({
+  loginMode,
+  email,
+  password,
+  invalidEmail,
+  invalidPassword,
+  handleModeSwitch,
+  handleChange,
+  handleSubmit
+}) {
+  return (
+    <form onSubmit={handleSubmit} className={styles.auth_form}>
+      <p>Please {loginMode ? "Sign In" : "Sign Up"} to continue:</p>
+      <div className={styles.form_control}>
+        <label>
+          Email
+          <input
+            type="text"
+            name="email"
+            value={email}
+            required
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      {!loginMode && invalidEmail && (
+        <p className={styles.invalid}>This is not a valid email address</p>
+      )}
+      <div className={styles.form_control}>
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            value={password}
+            required
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      {!loginMode && invalidPassword && (
+        <p className={styles.invalid}>Your password is not long enough</p>
+      )}
+      {loginMode && invalidPassword && <p className={styles.invalid}>Incorrect signin information</p>}
+      {loginMode && invalidEmail && <p className={styles.invalid}>Incorrect signin information</p>}
+      <div className={styles.form_actions}>
+        <button type="submit">Continue</button>
+        <button type="button" onClick={handleModeSwitch}>
+          Switch to {loginMode ? "Sign Up" : "Sign In"}
+        </button>
+      </div>
+    </form>
+  );
+}
