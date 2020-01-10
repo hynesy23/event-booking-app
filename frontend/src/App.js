d870087aa571da9f7540b3ca26cd258f2a9afcc6
Cillian <hynesy23@hotmail.com>
2020-01-10 17:34:14 +0000

    add Redirect component to send user to /auth from /

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./App.css";
-import { Router } from "@reach/router";
-import AuthPage from "./pages/AuthPage";
+import { Router, Redirect } from "@reach/router";
+import AuthPage from "./pages/auth/AuthPage";
 import BookingsPage from "./pages/BookingsPage";
 import EventsPage from "./pages/EventsPage";
+import MainNav from "./components/navigation/MainNav";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <AuthPage path="/auth" />
-        <BookingsPage path="/bookings" />
-        <EventsPage path="/events" />
-      </Router>
+      <MainNav />
+      <main className="main">
+        <Router>
+          <Redirect noThrow={true} from="/" to="auth" />
+          <AuthPage path="auth" />
+          <BookingsPage path="bookings" />
+          <EventsPage path="events" />
+        </Router>
+      </main>
     </div>
   );
 }
+
+// Line 15: Error kept appearing when using Redirect. According to reach/router github, need 'noThrow' in Redirect component to prevent this.
+// It is possible it kept erroring out as I was not using componentDidCatch (according to Reach Routers site)
 
 export default App;
