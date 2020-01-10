dfabc7ad31c5138facc8815388c65df58fbd77dd
Cillian <hynesy23@hotmail.com>
2020-01-10 13:31:00 +0000

    requestBody will vary depending on whether loginMode true or not

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-let requestBody;
-
-export const signInUser = (email, password) => {
+export const userLoginAction = (email, password, loginMode) => {
   console.log("hello");
-  requestBody = {
+
+  let requestBody = {
     query: `
     mutation { createUser(userInput: {email: "${email}", password: "${password}"}) {
       _id
@@ -13,17 +12,34 @@ export const signInUser = (email, password) => {
     }`
   };
 
+  if (loginMode) {
+    requestBody = {
+      query: `
+      query { login(email: "${email}", password: "${password}") {
+        userId
+        token
+        tokenExpiration
+      }
+      }`
+    };
+  }
+
   // return axios
   //   .post
-    return fetch("http://localhost:8000/graphql", {
-      method: 'POST',
-      body: JSON.stringify(requestBody),
-      headers: {
-        "Content-Type": "application/json"
-      }
+  return fetch("http://localhost:8000/graphql", {
+    method: "POST",
+    body: JSON.stringify(requestBody),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => {
+      console.log(res, "res log");
+      return res.json(); // Need this as using 'fetch' rather than axios.
     })
-    .then(response => {
-      console.log(response, "data log");
+    .then(parsedRes => {
+      console.log(parsedRes, "parsed result log");
+      return parsedRes;
     })
     .catch(err => {
       console.log(err);
@@ -31,4 +47,25 @@ export const signInUser = (email, password) => {
     });
 };
 
-export const createNewUser = () => {}
+// export const siginInUser = (email, password) => {
+//   console.log("api Signin");
+//   requestBody = {
+//     query: `
+//     query { login(email: "${email}", password: "${password}") {
+//       userId
+//       token
+//       tokenExpiration
+//     }
+//     }`
+//   };
+
+//   return fetch("http://localhost:8000/graphql", {
+//     method: "POST",
+//     body: JSON.stringify(requestBody),
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   }).then(response => {
+//     console.log(response, "api sign in response");
+//   });
+// };
