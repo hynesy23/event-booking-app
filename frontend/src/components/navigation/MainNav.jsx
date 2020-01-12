4ed87a0b12db5d1840bb2248f8f48878445cbce0
Cillian <hynesy23@hotmail.com>
2020-01-12 10:32:24 +0000

    add .Consumer and conditionally render links in Navbar based on token being present

--- frontend/src/components/navigation/MainNav.jsx
+++ frontend/src/components/navigation/MainNav.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import { Link } from "@reach/router";
+import AuthContext from "../../context/AuthContext";
 
 import * as styles from "./MainNav.module.css";
 
 export default function MainNav() {
   return (
-    <header className={styles.main_nav}>
-      <div className={styles.main_nav_logo}>
-        <h1>Easi-Book</h1>
-      </div>
-      <nav className={styles.main_nav_items}>
-        <ul>
-          <li>
-            <Link to="/auth">Auth</Link>
-          </li>
-          <li>
-            <Link to="/events">Events</Link>
-          </li>
-          <li>
-            <Link to="/bookings">Bookings</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <AuthContext.Consumer>
+      {context => (
+        <header className={styles.main_nav}>
+          <div className={styles.main_nav_logo}>
+            <h1>Easi-Book</h1>
+          </div>
+          <nav className={styles.main_nav_items}>
+            <ul>
+              {!context.token && (
+                <li>
+                  <Link to="/auth">Auth</Link>
+                </li>
+              )}
+              <li>
+                <Link to="/events">Events</Link>
+              </li>
+              {context.token && (
+                <li>
+                  <Link to="/bookings">Bookings</Link>
+                </li>
+              )}
+            </ul>
+          </nav>
+        </header>
+      )}
+    </AuthContext.Consumer>
   );
 }
