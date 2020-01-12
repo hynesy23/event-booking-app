0529cf3269cc9bb6efe74c5076cd7fcf5b7e6011
Cillian <hynesy23@hotmail.com>
2020-01-12 10:34:15 +0000

    refactor to class Comp and set token, userId in state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { Router, Redirect } from "@reach/router";
 import AuthPage from "./pages/auth/AuthPage";
 import BookingsPage from "./pages/BookingsPage";
 import EventsPage from "./pages/EventsPage";
 import MainNav from "./components/navigation/MainNav";
+import AuthContext from "./context/AuthContext";
 
-function App() {
-  return (
-    <div className="App">
-      <MainNav />
-      <main className="main">
-        <Router>
-          <Redirect noThrow={true} from="/" to="auth" />
-          <AuthPage path="auth" />
-          <BookingsPage path="bookings" />
-          <EventsPage path="events" />
-        </Router>
-      </main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    token: null,
+    userId: null
+  };
+
+  login = (token, userId) => {
+    console.log(token, userId, "token and user ID");
+    this.setState({ token, userId });
+  };
+
+  logout = () => {
+    this.setState({ token: null, userId: null });
+  };
+
+  render() {
+    const { token, userId } = this.state;
+    return (
+      <div className="App">
+        <AuthContext.Provider
+          value={{
+            token: token,
+            userId: userId,
+            login: this.login,
+            logout: this.logout
+          }}
+        >
+          <MainNav />
+          <main className="main">
+            <Router>
+              {!token && <Redirect noThrow={true} from="/" to="auth" exact />}
+              {token && <Redirect noThrow={true} from="/" to="events" exact />}
+              {token && (
+                <Redirect noThrow={true} from="auth" to="events" exact />
+              )}
+              {!token && <AuthPage path="auth" />}
+              {token && <BookingsPage path="bookings" />}
+              <EventsPage path="events" />
+            </Router>
+          </main>
+        </AuthContext.Provider>
+      </div>
+    );
+  }
 }
 
 // Line 15: Error kept appearing when using Redirect. According to reach/router github, need 'noThrow' in Redirect component to prevent this.
